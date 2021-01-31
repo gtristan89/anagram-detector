7ea2b4ae9a4ea275ace8d223b585fbac107d5808
Tristan Gibbs <gtristan89@gmail.com>
2021-01-30 18:37:24 -0700

    commit tree 55aa47cb15f498dd033ac4f1461bac5cb4f6e4c5

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+
+    def match(words)
+        words.select do |array|
+            (@word.split("").sort) == (array.split("").sort)
+        end
+    end
+end
